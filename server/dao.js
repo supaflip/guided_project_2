61e582149d83afbe0c9658d68be464bf7adc83ad
My Name <me@example.com>
2023-10-03 16:19:36 -0400

    Finished last DAO call: get_attr

--- server/dao.js
+++ server/dao.js
@@ -32,13 +32,21 @@ module.exports.call = async function call(operation, params, callback) {
                 break;
             }
 
-            
-
         case 'get_attr':
             try{
-                const collection = db.collection(params.collection)
-                const doc = await collection.findOne({id: +params.id});
-                callback({ body: doc[params.attr], status: 200 })
+                if (params.collection == 'films') {
+                    params.collection = params.attr;
+                    params.attr = 'films';
+                }
+
+                const collection = db.collection(`${params.attr}_${params.collection}`);
+                const key = `${params.collection.slice(0, -1)}_id`;
+                const docs = await collection.find({ [key]: + params.id }).toArray();
+
+                const attr_key = `${params.attr.slice(0, -1)}_id`;
+                const attr_collection = db.collection(params.attr);
+                const attr_docs = await Promise.all(docs.map(async (doc) => await attr_collection.findOne({id: +doc[attr_key]})));
+                callback({ body: attr_docs, status: 200 });
             } catch (e) {
                 console.log(e);
                 callback({ body: false, status: 404});
