35589316ff1389729f34fc29235d842a044b49e0
My Name <me@example.com>
2023-10-03 13:44:29 -0400

    Initial express endpoints

--- server/dao.js
+++ server/dao.js
@@ -1,11 +1,32 @@
 const { MongoClient } = require('mongodb');
 
 const url = "mongodb://localhost:2707";
-const client = MongoClient(url);
+const client = new MongoClient(url);
 const db_name = "swapi";
 
 module.exports.call = async function call(operation, params, callback) {
     const db = client.db(db_name);
 
-    // do stuff
-}
+    switch(operation.toLowerCase()) {
+        case 'get_all':
+            callback({ body: [params.collection]});
+            break;
+        
+        case 'get_one':
+            callback({ body: [params.collection, params.id]});
+            break;
+
+        case 'get_attr':
+            callback({ body: [params.collection, params.id, params.attr]})
+            break;
+
+        default:
+            callback({ status: "requested data not found", body: params });
+            break;
+    };
+
+    console.log('call complete: ' + operation);
+    client.close();
+    return 'call complete';
+};
+
--- server/server.js
+++ server/server.js
@@ -1,9 +1,79 @@
 const express = require('express');
-const dao = require('./dao.js')
+const dao = require('./dao.js');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 
-// do things
+function queryDAO(operation, req, res) {
+    dao.call(operation, req.params, (result) => {
+        if (result.body !== undefined) {
+            console.log(result.body);
+            res.send(result.body);
+        } else {
+            res.statusCode = 404;
+            res.end;
+        };
+    }); 
+}
+
+app.get("/:collection/:id/:attr", (req, res) => {
+    queryDAO('get_attr', req, res);
+});
+
+app.get("/:collection/:id", (req, res) => {
+    queryDAO('get_one', req, res);
+});
+
+app.get("/:collection", (req, res) => {
+    queryDAO('get_all', req, res); 
+});
+
+/*
+app.get("/films", (req, res) => {
+    return getAll('films', req);
+});
+
+app.get("/films/:id", (req, res) => {
+    dao.call('film', req, (result) => {
+        console.log('result: ' + result.status);
+    });
+});
+
+app.get("/films/:id/chracters", (req, res) => {
+    dao.call('film', req, (result) => {
+        console.log('result: ' + result.status);
+    });
+});
+
+app.get("/films/:id/planets", (req, res) => {
+    dao.call('film', req, (result) => {
+        console.log('result: ' + result.status);
+    });
+});
+
+app.get("/planets", (req, res) => {
+    return getAll('planets',)
+});
+
+app.get("/planets/:id", (req, res) => {
+    dao.call('planet', req, (result) => {
+        console.log('result: ' + result.status);
+    });
+});
+
+app.get("/planets/:id/films", (req, res) => {
+    dao.call('planet_films', req, (result) => {
+        console.log('result: ' + result.status);
+    });
+});
+
+app.get("/planets/:id/chracters", (req, res) => {
+    dao.call('planet_characters', req, (result) => {
+        console.log('result: ' + result.status);
+    });
+});
+*/
 
 const port = 3000;
 console.log('server starting on port ' + port);
